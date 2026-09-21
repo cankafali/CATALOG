28bde470b42dcde1efbcc3bc4f414747f7cf6231
Hakkı Can Kafalı <hakkican.kafali@gmail.com>
2026-09-21 16:04:05 +0300

    favori butonu olusturuldu

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ProductCard from './components/ProductCard'
 
 function App() {
   const [arama, setArama] = useState("");
+  const [favori, setFavori] = useState([]);
 
   const urunler = [
     { id: 1, isim: "kulaklık", fiyat: 1200, gorsel: "https://picsum.photos/id/10/300" },
@@ -10,6 +11,9 @@ function App() {
     { id: 3, isim: "kmouse", fiyat: 1600, gorsel: "https://picsum.photos/id/30/300" },
     { id: 4, isim: "pc", fiyat: 2200, gorsel: "https://picsum.photos/id/40/300" }
   ]
+  function favEkle(id) {
+    setFavori([...favori, id])
+  }
 
   const filtrelenmisUrunler = urunler.filter((u) => (
     u.isim.toLocaleLowerCase('tr').includes(arama.toLocaleLowerCase('tr'))
@@ -26,13 +30,14 @@ function App() {
             setArama(e.target.value);
           }}
         />
+        <p>Favoriler: {favori.join(", ")}</p>
         <p>Aranan: {arama}</p>
       </div>
       {filtrelenmisUrunler.length === 0 && <p>Ürün bulunamadı</p>}
       <div className='flex flex-wrap gap-6'>
         {filtrelenmisUrunler.map(
           (u) => (
-            <ProductCard key={u.id} isim={u.isim} fiyat={u.fiyat} link={u.gorsel} />
+            <ProductCard key={u.id} id={u.id} isim={u.isim} fiyat={u.fiyat} link={u.gorsel} favEkle={favEkle} />
           )
         )}
       </div>
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,8 @@
-function ProductCard({ isim, fiyat, link }) {
+function ProductCard({ key, id, isim, fiyat, link, favEkle }) {
     return (
         <div className="w-72 overflow-hidden rounded-xl border border-gray-200 bg-white shadow-sm">
             <div>
+                <p>{id}</p>
                 <img
                     src={link}
                     alt="Ürün görseli"
@@ -13,12 +14,11 @@ function ProductCard({ isim, fiyat, link }) {
                     <p className="font-medium text-gray-700">{isim}</p>
                     <p className="text-lg font-bold text-gray-900">{fiyat}</p>
                 </div>
-                <button className="w-full rounded-lg bg-gray-900 py-2 text-white hover:bg-gray-700">
-<<<<<<< HEAD
-                    satın al
-=======
+                <button className="cursor-pointer transition active:scale-95 w-full rounded-lg bg-gray-900 py-2 text-white hover:bg-gray-700">
                     Sepete at
->>>>>>> feature/buton
+                </button>
+                <button onClick={() => favEkle(id)} className="cursor-pointer transition active:scale-95 w-full rounded-lg border border-gray-300 py-2 text-gray-700 hover:bg-gray-50" >
+                    Favoriye ekle
                 </button>
             </div>
         </div>
