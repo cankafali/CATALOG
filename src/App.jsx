aa2f7f3c0fd847fb8e798c2795bd360922d929b8
Hakkı Can Kafalı <hakkican.kafali@gmail.com>
2026-09-21 13:39:44 +0300

    props ile dinamik hale getirildi productcard

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import ProductCard from './components/ProductCard'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 p-8">
+    <div className="min-h-screen bg-gray-100 p-8 flex gap-6">
 
-
-      <ProductCard />
+      <ProductCard isim="kulaklık" fiyat="1200" link="https://picsum.photos/id/10/300" />
+      <ProductCard isim="klavye" fiyat="200" link="https://picsum.photos/id/20/300" />
     </div>
   )
 }
