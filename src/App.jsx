31ad145f17cc8e964bd20c38701627fb6fe6a5a8
Hakkı Can Kafalı <hakkican.kafali@gmail.com>
2026-09-21 13:00:51 +0300

    iskelet olusturuldu

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import ProductCard from './components/ProductCard'
+
 function App() {
   return (
-    <div className="text-3xl font-bold text-red-500">
-      Tailwind çalışıyor
+    <div >
+
+
+      <ProductCard />
     </div>
   )
 }
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,19 @@
+function ProductCard() {
+
+
+    return (
+        <div>
+            <div>
+                <img src="https://picsum.photos/300" />
+            </div>
+            <div>
+                <div>
+                    <p>isim</p>
+                    <p>fiyat</p>
+                </div>
+                <button onclick="">sepete ekle</button>
+            </div>
+        </div>
+    )
+}
+export default ProductCard
